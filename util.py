4200b75df4bc1bad4a6e5c80323099b65c60728f
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2017-01-10 18:02:33 +0200

    edge detection implemented

--- util.py
+++ util.py
@@ -2,6 +2,7 @@
 # This module compares two images and estimates its similarity
 #
 import numpy as np
+from scipy import ndimage, misc
 
 
 def similarity(img, q_profile): 
@@ -14,8 +15,20 @@ def similarity(img, q_profile):
 
 
 def get_profile(img): 
-	# TODO: define a structure for image pofiling
-	# 	and then return the profile of a given image
-	return 0.
+	""" The function returns a list of points in 4-dimensional space (ro, th, t1, t2),
+		which represent the edges extracted from the image, where
+		(ro, th) - is a line in polar referrence frame and
+		t1, t2 - two scalars defining an edge laying in this line
+	"""
+	gray = np.average(img[:,:,:3],axis=2)
+	print('gray shape:',gray.shape)
+	mask = np.array([[1,0,-1],[2,0,-2],[1,0,-1]])
+	edges = np.zeros(gray.shape)
+	edges = abs(ndimage.convolve(gray,mask))
+
+	#sx = ndimage.sobel(img, axis=0, mode='constant')
+	#sy = ndimage.sobel(img, axis=1, mode='constant')
+	#sob = np.hypot(sx, sy)
+	misc.imsave('8.png',edges)
 
 
